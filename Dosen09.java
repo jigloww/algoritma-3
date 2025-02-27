ff0145c506b3ca29a0b462b0adf3811276265f7c
unknown <bima.rafi60@sma.belajar.id>
2025-02-27 19:03:20 +0700

    hmm

--- Dosen09.java
+++ Dosen09.java
@@ -1,26 +1,82 @@
 import java.util.Scanner;
 
-public class Dosen09 {
+class Dosen09 {
     String kode;
     String nama;
     Boolean jenisKelamin; 
     int usia;
 
-    
     public Dosen09(String kode, String nama, Boolean jenisKelamin, int usia) {
         this.kode = kode;
         this.nama = nama;
         this.jenisKelamin = jenisKelamin;
         this.usia = usia;
     }
-
-    
-    public void cetakInfo() {
-        System.out.println("Kode           : " + this.kode);
-        System.out.println("Nama           : " + this.nama);
-        System.out.println("Jenis Kelamin  : " + (this.jenisKelamin ? "Pria" : "Wanita"));
-        System.out.println("Usia           : " + this.usia);
-        System.out.println("=================================");
-    }
 }
 
+class DataDosen {
+    
+    public static void dataSemuaDosen(Dosen09[] arrayOfDosen) {
+        for (Dosen09 d : arrayOfDosen) {
+            System.out.println("Kode          : " + d.kode);
+            System.out.println("Nama          : " + d.nama);
+            System.out.println("Jenis Kelamin : " + (d.jenisKelamin ? "Pria" : "Wanita"));
+            System.out.println("Usia          : " + d.usia);
+            System.out.println("-----------------------------");
+        }
+    }
+
+    
+    public static void jumlahDosenPerJenisKelamin(Dosen09[] arrayOfDosen) {
+        int pria = 0, wanita = 0;
+        for (Dosen09 d : arrayOfDosen) {
+            if (d.jenisKelamin) pria++;
+            else wanita++;
+        }
+        System.out.println("Jumlah Dosen Pria   : " + pria);
+        System.out.println("Jumlah Dosen Wanita : " + wanita);
+    }
+
+    public static void rerataUsiaDosenPerJenisKelamin(Dosen09[] arrayOfDosen) {
+        int totalPria = 0, totalWanita = 0;
+        int countPria = 0, countWanita = 0;
+
+        for (Dosen09 d : arrayOfDosen) {
+            if (d.jenisKelamin) {
+                totalPria += d.usia;
+                countPria++;
+            } else {
+                totalWanita += d.usia;
+                countWanita++;
+            }
+        }
+        System.out.println("Rata-rata Usia Dosen Pria   : " + (countPria == 0 ? 0 : (double) totalPria / countPria));
+        System.out.println("Rata-rata Usia Dosen Wanita : " + (countWanita == 0 ? 0 : (double) totalWanita / countWanita));
+    }
+
+   
+    public static void infoDosenPalingTua(Dosen09[] arrayOfDosen) {
+        Dosen09 tertua = arrayOfDosen[0];
+        for (Dosen09 d : arrayOfDosen) {
+            if (d.usia > tertua.usia) {
+                tertua = d;
+            }
+        }
+        System.out.println("Dosen Paling Tua:");
+        System.out.println("Nama  : " + tertua.nama);
+        System.out.println("Usia  : " + tertua.usia);
+    }
+
+    
+    public static void infoDosenPalingMuda(Dosen09[] arrayOfDosen) {
+        Dosen09 termuda = arrayOfDosen[0];
+        for (Dosen09 d : arrayOfDosen) {
+            if (d.usia < termuda.usia) {
+                termuda = d;
+            }
+        }
+        System.out.println("Dosen Paling Muda:");
+        System.out.println("Nama  : " + termuda.nama);
+        System.out.println("Usia  : " + termuda.usia);
+    }
+}
--- DosenDemo09.java
+++ DosenDemo09.java
@@ -1,37 +1,42 @@
 import java.util.Scanner;
-
 public class DosenDemo09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukkan jumlah dosen: ");
-        int jumlahDosen = Integer.parseInt(sc.nextLine());
+        int jumlah = sc.nextInt();
+        sc.nextLine(); // Flush newline
 
-        Dosen09[] daftarDosen = new Dosen09[jumlahDosen];
+        Dosen09[] arrayOfDosen = new Dosen09[jumlah];
 
-        for (int i = 0; i < jumlahDosen; i++) {
-            System.out.println("Masukkan Data Dosen ke-" + (i + 1));
-
-            System.out.print("Kode           : ");
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("\nMasukkan Data Dosen ke-" + (i + 1));
+            System.out.print("Kode          : ");
             String kode = sc.nextLine();
-
-            System.out.print("Nama           : ");
+            System.out.print("Nama          : ");
             String nama = sc.nextLine();
+            System.out.print("Jenis Kelamin (Pria = true, Wanita = false) : ");
+            Boolean jenisKelamin = sc.nextBoolean();
+            System.out.print("Usia          : ");
+            int usia = sc.nextInt();
+            sc.nextLine(); // Flush newline
 
-            System.out.print("Jenis Kelamin (L/P) : ");
-            char gender = sc.nextLine().charAt(0);
-            Boolean jenisKelamin = (gender == 'L' || gender == 'l'); 
-
-            System.out.print("Usia           : ");
-            int usia = Integer.parseInt(sc.nextLine());
-
-            daftarDosen[i] = new Dosen09(kode, nama, jenisKelamin, usia);
+            arrayOfDosen[i] = new Dosen09(kode, nama, jenisKelamin, usia);
         }
 
-        
-        System.out.println("\nData Dosen yang Telah Dimasukkan:");
-        for (Dosen09 dosen : daftarDosen) {
-            dosen.cetakInfo();
-        }
+        System.out.println("\n===== DATA SEMUA DOSEN =====");
+        DataDosen.dataSemuaDosen(arrayOfDosen);
+
+        System.out.println("\n===== JUMLAH DOSEN PER JENIS KELAMIN =====");
+        DataDosen.jumlahDosenPerJenisKelamin(arrayOfDosen);
+
+        System.out.println("\n===== RATA-RATA USIA DOSEN PER JENIS KELAMIN =====");
+        DataDosen.rerataUsiaDosenPerJenisKelamin(arrayOfDosen);
+
+        System.out.println("\n===== DOSEN PALING TUA =====");
+        DataDosen.infoDosenPalingTua(arrayOfDosen);
+
+        System.out.println("\n===== DOSEN PALING MUDA =====");
+        DataDosen.infoDosenPalingMuda(arrayOfDosen);
     }
 }
